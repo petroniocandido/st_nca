a9bf79d3024d56d7532ab1b3cda31186939b9086
Petrônio Cândido de Liam e Silva <petronio.candido@gmail.com>
2024-11-27 14:48:33 -0300

    Fine Tunning methods

--- st_nca/finetune.py
+++ st_nca/finetune.py
@@ -1,29 +1,47 @@
-from datetime import datetime, timezone, timedelta
+import copy
+import time
 import numpy as np
 import pandas as pd
 import networkx as nx
+import matplotlib.pyplot as plt
+from datetime import datetime, timezone, timedelta
 
 import torch
 from torch import nn, optim
 from torch.nn import functional as F
 from torch.utils.data import Dataset, DataLoader
+from torchmetrics.regression import SymmetricMeanAbsolutePercentageError
 
 from tensordict import TensorDict
 
-from st_nca.cellmodel import CellModel
+from st_nca.common import DEVICE, checkpoint
+from st_nca.datasets import PEMS03
+from st_nca.gca import get_timestamp
+from st_nca.embeddings.temporal import from_datetime_to_np, from_np_to_datetime
 from st_nca.tokenizer import NeighborhoodTokenizer
 
 
 class FineTunningDataset(Dataset):
-  def __init__(self, pems, sensors, **kwargs):
+  def __init__(self, pems, sensors, train = 0.7, **kwargs):
     super().__init__()
 
-    self.pems = pems
+    self.pems : PEMS03 = pems
     self.sensors = sensors
     self.num_nodes = len(sensors)
     self.increment_type = kwargs.get('increment_type','minute')
     self.increment = kwargs.get('increment',1)
     self.steps_ahead = kwargs.get('steps_ahead',10)
+
+    self.num_samples = self.pems.num_samples
+
+    self.index = np.random.shuffle([k for k in range(self.num_samples)])
+
+    self.train_split = int(train * self.num_samples)
+
+    self.is_validation = False
+
+    self.start = 0
+    self.end = self.num_samples
 
   def __getitem__(self, date):
     if isinstance(date, datetime):
@@ -50,13 +68,29 @@ class FineTunningDataset(Dataset):
       date = from_np_to_datetime(dt2)
      
     return X,y
+  
+  def train(self):
+    tmp = copy.deepcopy(self)
+    tmp.is_validation = False
+    tmp.start = 0
+    tmp.end = self.train_split
+    tmp.num_samples = self.train_split
+    return tmp
+
+  def test(self):
+    tmp = copy.deepcopy(self)
+    tmp.is_validation = True
+    tmp.start = self.train_split
+    tmp.end = self.num_samples
+    tmp.num_samples = self.num_samples - self.train_split
+    return tmp
 
   def __len__(self):
-    return self.pems.num_samples
+    return self.num_samples
 
   def __iter__(self):
-    for date in self.pems.data['timestamp']:
-      yield self[date]
+    for ix in self.index[self.start, self.end]:
+      yield self[self.pems.data['timestamp'][ix]]
 
   def to(self, *args, **kwargs):
     if isinstance(args[0], str):
@@ -65,3 +99,139 @@ class FineTunningDataset(Dataset):
       self.dtype = args[0]
     self.pems = self.pems.to(*args, **kwargs)
     return self
+
+
+
+def finetune_step(DEVICE, train, test, model, loss, mape, optim, **kwargs):
+
+  iterations = kwargs.get('iterations',1)
+  increment_type = kwargs.get('increment_type','minute')
+  increment = kwargs.get('increment',1)
+  batch = kwargs.get('batch',10)
+
+  model.train()
+
+  errors = []
+  mapes = []
+  for ct in range(batch):
+    error = torch.tensor([0], dtype=model.dtype, device=model.device)
+    map = torch.tensor([0], dtype=model.dtype, device=model.device)
+    for X,y in train:
+
+      initial_date = X['timestamp']
+
+      optim.zero_grad()
+
+      X = X.to(DEVICE)
+      y = y.to(DEVICE)
+
+      y_pred = model.run(initial_date = initial_date, initial_state = X, iterations = iterations,
+                        increment_type = increment_type, increment = increment, return_type = 'tensor')
+
+      error += loss(y, y_pred.squeeze())
+      map += mape(y, y_pred.squeeze())
+
+    error.backward()
+    optim.step()
+
+    # Grava as métricas de avaliação
+    errors.append(error.cpu().item())
+    mapes.append(map.cpu().item())
+
+
+  ##################
+  # VALIDATION
+  ##################
+
+  model.eval()
+
+  errors_val = []
+  mapes_val = []
+  with torch.no_grad():
+    for ct in range(batch):
+      error_val = torch.tensor([0], dtype=model.dtype, device=model.device)
+      map_val = torch.tensor([0], dtype=model.dtype, device=model.device)
+      for X,y in test:
+
+        initial_date = X['timestamp']
+
+        X = X.to(DEVICE)
+        y = y.to(DEVICE)
+
+        y_pred = model.run(initial_date = initial_date, initial_state = X, iterations = iterations,
+                        increment_type = increment_type, increment = increment, return_type = 'tensor')
+
+        error_val += loss(y, y_pred.squeeze())
+        map_val += mape(y, y_pred.squeeze())
+
+      errors_val.append(error_val.cpu().item())
+      mapes_val.append(map_val.cpu().item())
+
+  return errors, mapes, errors_val, mapes_val
+
+
+def finetune_loop(DEVICE, dataset, model, display = None, **kwargs):
+
+  model = model.to(DEVICE)
+
+  checkpoint_file = kwargs.get('checkpoint_file', 'modelo.pt')
+
+  if display is None:
+    from IPython import display
+
+  batch_size = kwargs.get('batch', 10)
+
+  fig, ax = plt.subplots(1,3, figsize=(15, 5))
+
+  epochs = kwargs.get('epochs', 10)
+  lr = kwargs.get('lr', 0.001)
+  optimizer = kwargs.get('optim', optim.Adam(model.parameters(), lr=lr, weight_decay=0.0005))
+
+  train_ldr = DataLoader(dataset.train(), batch_size=batch_size, shuffle=True)
+  test_ldr = DataLoader(dataset.test(), batch_size=batch_size, shuffle=True)
+
+  loss = nn.MSELoss()
+  mape = SymmetricMeanAbsolutePercentageError().to(DEVICE)
+
+  error_train = []
+  mape_train = []
+  error_val = []
+  mape_val = []
+
+  start_time = time.time()
+
+  for epoch in range(epochs):
+
+    if epoch % 5 == 0:
+      checkpoint(model, checkpoint_file)
+
+    errors_train, map_train, errors_val, map_val = finetune_step(DEVICE, train_ldr, test_ldr, 
+                                                                 model, loss, mape, optimizer, **kwargs)
+
+    error_train.append(np.mean(errors_train))
+    mape_train.append(np.mean(map_train))
+    error_val.append(np.mean(errors_val))
+    mape_val.append(np.mean(map_val))
+
+    display.clear_output(wait=True)
+    ax[0].clear()
+    ax[0].plot(error_train, c='blue', label='Train')
+    ax[0].plot(error_val, c='red', label='Test')
+    ax[0].legend(loc='upper left')
+    ax[0].set_title("LOSS - All Epochs {} - Time: {} s".format(epoch, round(time.time() - start_time, 0)))
+    ax[1].clear()
+    ax[1].plot(error_train[-20:], c='blue', label='Train')
+    ax[1].plot(error_val[-20:], c='red', label='Test')
+    ax[1].set_title("LOSS - Last 20 Epochs".format(epoch))
+    ax[1].legend(loc='upper left')
+    ax[2].clear()
+    ax[2].plot(mape_train[-20:], c='blue', label='Train')
+    ax[2].plot(mape_val[-20:], c='red', label='Test')
+    ax[2].set_title("MAPE - Last 20 Epochs".format(epoch))
+    ax[2].legend(loc='upper left')
+    plt.tight_layout()
+    display.display(plt.gcf())
+
+  plt.savefig(checkpoint_file+".pdf", dpi=150)
+
+  checkpoint(model, checkpoint_file)
